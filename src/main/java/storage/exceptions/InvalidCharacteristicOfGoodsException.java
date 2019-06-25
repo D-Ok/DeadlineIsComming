c0e9d4e86106e1cdce0f6575710b7b32188ad39c
Daryna Oliinyk <darusya172@gmail.com>
2019-06-25 12:51:30 +0300

    database added

---
+++ src/main/java/storage/exceptions/InvalidCharacteristicOfGoodsException.java
@@ -0,0 +1,12 @@
+package storage.exceptions;
+
+public class InvalidCharacteristicOfGoodsException extends Exception {
+	public InvalidCharacteristicOfGoodsException() {
+		super();
+	}
+	
+	public InvalidCharacteristicOfGoodsException(String e) {
+		super(e);
+	}
+}
+
